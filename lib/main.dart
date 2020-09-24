4239e4e96c7662e113f618c943adc48126debc7d
yunweneric <yunweneric@gmail.com>
2020-09-24 11:32:47 +0100

    added system status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'package:find_docky/screens/find.dart';
-// import 'package:find_docky/screens/find.dart';
 import 'package:find_docky/screens/home_screen.dart';
 import 'package:find_docky/screens/login.dart';
 import 'package:find_docky/screens/signup.dart';
@@ -7,21 +5,26 @@ import 'package:find_docky/screens/welcome.dart';
 import 'package:find_docky/screens/welcome2.dart';
 import 'package:find_docky/screens/welcome3.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-// import 'screens/welcome.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.blue),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // primaryColor: Color(value),
-        primaryColor: Color(0xFFF3F6FC),
+        primaryColor: Colors.blue,
+        backgroundColor: Colors.blue,
+        // primaryColor: Color(0xFFF3F6FC),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // home: FindState()
@@ -33,7 +36,7 @@ class MyApp extends StatelessWidget {
         "/third": (context) => ThirdWelcome(),
         "/signup": (context) => SignUp(),
         "/login": (context) => LogIn(),
-        "/home" : (context) => HomeScreen(),
+        "/home": (context) => HomeScreen(),
       },
     );
   }
